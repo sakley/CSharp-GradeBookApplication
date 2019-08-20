c478a365f407289c36558ad04c1bda9f9c530e03
sakley <sacha.kley@melix.org>
2019-08-20 15:19:22 +0200

    Students get Ranked LetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,10 +14,32 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            var studentCount = Students.Count;
+            int studentCount = Students.Count;
             if (studentCount < 5)
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            int rank = 0;
+            foreach(Student student in this.Students)
+            {
+                if (averageGrade < student.AverageGrade)
+                {
+                    rank++;
+                }
+            }
+           int rankSize = studentCount / 5;
+           int percentile = rank/rankSize;
+            switch(percentile)
+            {
+                case 0:
+                    return ('A');
+                case 1:
+                    return ('B');
+                case 2:
+                    return ('C');
+                case 3:
+                    return ('D');
             }
             return ('F');
         }
